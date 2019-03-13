6e800d4275a836ef00d2c7c99ebc0074b00c1e7c
njenga <njengak@me.com>
2019-03-13 11:32:39 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+
+  #CREATE
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @created_rec = Recipe.create(params)
+    redirect "/recipes/#{@created_rec.id}"
+  end
+
+  #INDEX/SHOW
+  get '/recipes' do
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @lookup_recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  #UPDATE
+  get '/recipes/:id/edit' do
+    @lookup_recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @lookup_recipe = Recipe.find(params[:id])
+    @lookup_recipe.update(params[:recipe])
+    redirect "/recipes/#{@lookup_recipe.id}"
+  end
+
+  #DELETE
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect :index
+  end
+
 
 end
